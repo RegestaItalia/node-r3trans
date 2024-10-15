0d281c7b79b6f6340972f4a6ccb64869d6b97dbd
simonegaffurini <simone.gaffurini@regestaitalia.it>
2024-10-15 20:49:31 +0200

    remove preinstall (will fix)

--- _preinstall.js
+++
@@ -1,3 +0,0 @@
-console.log(`Module "node-r3trans" requires "R3trans" program.`);
-console.log(`-> You can download "R3trans" from SAP Software Download Center (https://support.sap.com/en/my-support/software-downloads.html)`);
-console.log(`After download, either set environment variable "R3TRANS_HOME" to download location or manually indicate during use of "node-r3trans".`);
